dd1a51fc7594730d6a3f839eb193e852de2bc8e9
ManojNadar <manojviru03@gmail.com>
2023-05-25 11:12:29 +0530

    js satrts

---
+++ script.js
@@ -0,0 +1,39 @@
+// JS - Datatypes - String,number,Boolean
+
+// string - text- "legion", "manoj" , "123", "true" - string/text can be written inside "" , '', ``  quotations
+// number - number - 123, 1.5, 1000,20.5 - number can be written without "quotations"
+// Boolean - true , false - this can be written without "quotations"
+
+// variables can be used to store the data
+
+// var keyword is a container or box where we can store the string number boolean etc by giving own name
+
+
+// typeof is a predefined function.. it can  
+var myName = "manoj";
+console.log(myName);
+console.log(typeof myName);
+
+// var myName  = manoj   ----->  throws error except true and false all other text must be inside "quotations"
+
+var num = 123;
+
+// ----> Number
+console.log(num);
+console.log(typeof num);
+
+var num = "123";
+
+// ----> if number is written inside any "quotations" it is also considered as a string
+console.log(num);
+console.log(typeof num);
+
+var bool = true;
+// -----> boolean
+console.log(bool);
+console.log(typeof bool);
+
+var bool = "true";
+// ------>   if true or false written inside any "quotations" it can be considered as a string
+console.log(bool);
+console.log(typeof bool);
