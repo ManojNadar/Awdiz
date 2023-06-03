0e28249c4e5a32f359b295389f24714871e31fb2
ManojNadar <manojviru03@gmail.com>
2023-06-03 20:25:47 +0530

    02-06-2023 loops

--- js/01-06-loop.js
+++ js/01-06-loop.js
@@ -30,32 +30,59 @@
 
 // ASSIGNMENT
 
-let value = 100; //by subtracting 2 number
+// let value = 100; //by subtracting 2 number
 
-let marks = [120, 30, 40, 20, 500, 35, 70];
+// let marks = [120, 30, 40, 20, 500, 35, 70];
 
-for (let i = 0; i < marks.length; i++) {
-  for (let j = i + 1; j < marks.length; j++) {
-    if (marks[i] - marks[j] === value) {
-      console.log(marks[i], "-", marks[j], "=", value);
-    }
-  }
-}
+// for (let i = 0; i < marks.length; i++) {
+//   for (let j = i + 1; j < marks.length; j++) {
+//     if (marks[i] - marks[j] === value) {
+//       console.log(marks[i], "-", marks[j], "=", value);
+//     }
+//   }
+// }
 
 // bby adding three number get the answer 55
 
-let findNum = 55;
+// let findNum = 55;
 
-let num = [44, 33, 2, 11, 14, 35, 20, 2];
+// let num = [44, 33, 2, 11, 14, 35, 20, 2];
 
-for (i = 0; i < num.length; i++) {
-  //   console.log(num[i]);
-  for (j = i + 1; j < num.length; j++) {
-    // console.log(num[j]);
-    for (k = j + 1; k < num.length; k++) {
-      //   console.log(num[k]);
-      if (num[i] + num[j] + num[k] === findNum)
-        console.log(num[i], "+", num[j], "+", num[k], "=", findNum);
+// for (i = 0; i < num.length - 2; i++) {
+//   //   console.log(num[i]);
+//   for (j = i + 1; j < num.length - 1; j++) {
+//     // console.log(num[j]);
+//     for (k = j + 1; k < num.length; k++) {
+//       //   console.log(num[k]);
+//       if (num[i] + num[j] + num[k] === findNum) {
+//         console.log(num[i], "+", num[j], "+", num[k], "=", findNum);
+//       }
+//     }
+//   }
+// }
+
+// Find 4 numbers from array that addition is target. Use nested loop 4 times.
+
+var target = 25;
+var array = [
+  2, 3, 5, 5, 32, 54, 23, 12, 45, 23, 43, 1, 34, 65, 1, 54, 6, 33, 4, 9, 7,
+];
+
+// console.log(array.length);
+
+for (let i = 0; i < array.length - 3; i++) {
+  // console.log(array[i]);
+  for (let j = i + 1; j < array.length - 2; j++) {
+    // console.log(array[j]);
+    for (let k = j + 1; k < array.length - 1; k++) {
+      // console.log(array[k]);
+      for (let l = k + 1; l < array.length; l++) {
+        // console.log(array[l]);
+
+        if (array[i] + array[j] + array[k] + array[l] === target) {
+          console.log(i, "+", j, "+", k, "+", l, "=", target);
+        }
+      }
     }
   }
 }
