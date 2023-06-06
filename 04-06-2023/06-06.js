a301727a96c0c0ef4184ef50b118ae75dc0af6ea
ManojNadar <manojviru03@gmail.com>
2023-06-06 11:13:46 +0530

    awdiz 06-06

---
+++ 04-06-2023/06-06.js
@@ -0,0 +1,31 @@
+// function greet() {
+//   alert("Welcome to World");
+// }
+// function name() {
+//   alert("Manoj");
+// }
+// function skills() {
+//   alert("JavaScript");
+// }
+// function sports() {
+//   alert("Cricket");
+// }
+
+function add() {
+  let num = 2;
+  alert(`${num} + ${num} = ${num + num}`);
+}
+function sub() {
+  let num = 12;
+  let num2 = 2;
+  alert(`${num} - ${num2} = ${num - num2}`);
+}
+function mul() {
+  let num = 4;
+  alert("4 * 4 =" + num * num);
+}
+function div() {
+  let num = 16;
+  let num2 = 2;
+  alert("16 / 2 =" + num / num2);
+}
