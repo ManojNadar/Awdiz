403862af9b8fe75778c354d4b2f679608fe2fdcc
ManojNadar <manojviru03@gmail.com>
2023-06-07 19:20:50 +0530

    update js in google docs

--- js/27-05-script.js
+++ js/27-05-script.js
@@ -1,79 +1,78 @@
 // operators + - * /
 
-// var num1 = 25;
-// var num2 = 50;
+var num1 = 25;
+var num2 = 50;
 
-// addition
-// console.log(num1 + num2);
+addition;
+console.log(num1 + num2);
 
 // addition shorthand
-// var a = 10;
-// console.log(a);
+var a = 10;
+console.log(a);
 // updated a + 1
 
-// a = a + 1;
-// a+=1
-// console.log(a);
+a = a + 1;
+a += 1;
+console.log(a);
 // updated a + 1
-// a++;
-// console.log(a);
+a++;
+console.log(a);
 
-// //subtraction
-// console.log(num1 - num2);
+//subtraction
+console.log(num1 - num2);
 
-// //multiplication
-// console.log(num1 * num2);
+//multiplication
+console.log(num1 * num2);
 
-// //division
-// console.log(num1 / num2);
+//division
+console.log(num1 / num2);
 
-// //condition in console
-// // greater than
-// console.log(num1 > num2);
-// //lesser than
-// console.log(num1 < num2);
+//condition in console
+// greater than
+console.log(num1 > num2);
+//lesser than
+console.log(num1 < num2);
 
-// lesser && greater
-// var number = 50;
-// console.log(number > 100 && number < 70);
+lesser && greater;
+var number = 50;
+console.log(number > 100 && number < 70);
 
-// lesser  || greater
-// console.log(number > 25 || number < 100);
+lesser || greater;
+console.log(number > 25 || number < 100);
 
-// Condtional statement
+// Conditional statement
 
 // syntax -
 // if( conditions){
-//     code to be executed
+// code to be executed
 // } else if(condition 2){
-//     code to be executed
+// code to be executed
 // }else{
-//     code to be executed
+// code to be executed
 // }
 
 // Q. userAge 18 Learning License
 //     userAge less than 18 and greater than 80 no License
 //     userAge greater than 18 and lesser than 80 Driving License
 
-// var userAge = 18;
+var userAge = 18;
 
-// if (userAge === 18) {
-//   console.log("You are Eligible for Learning License");
-// } else if (userAge > 18 && userAge < 80) {
-//   console.log(" you are  eligible for Driving License");
-// } else {
-//   console.log("Sorry You are NOT eligible for  License");
-// }
+if (userAge === 18) {
+  console.log("You are Eligible for Learning License");
+} else if (userAge > 18 && userAge < 80) {
+  console.log(" you are  eligible for Driving License");
+} else {
+  console.log("Sorry You are NOT eligible for  License");
+}
 
-// var marks = 50;
+var marks = 50;
 
-// if (marks <= 34) {
-//   console.log("Sorry Failed");
-// } else if (marks >= 35 && marks <= 50) {
-//   console.log("Average");
-// } else if (marks > 50 && marks <= 75) {
-//   console.log("Good Marks");
-// } else {
-//   console.log("Excellent Marks");
-// }
-
+if (marks <= 34) {
+  console.log("Sorry Failed");
+} else if (marks >= 35 && marks <= 50) {
+  console.log("Average");
+} else if (marks > 50 && marks <= 75) {
+  console.log("Good Marks");
+} else {
+  console.log("Excellent Marks");
+}
