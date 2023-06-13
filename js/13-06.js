a9438d6526e54d8c5c45a83a75475d6db9b7359f
ManojNadar <manojviru03@gmail.com>
2023-06-13 11:06:44 +0530

    js async await promise

---
+++ js/13-06.js
@@ -0,0 +1,93 @@
+// console.log("first");
+// console.log("second");
+// console.log("third");
+
+// setTimeout(() => {
+//   console.log("first");
+// }, 2000);
+// setTimeout(() => {
+//   console.log("second");
+// }, 1000);
+// setTimeout(() => {
+//   console.log("third");
+// }, 1500);
+
+// ASync Await
+
+// function greetings() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Welcome to Awdiz");
+//     }, 3000);
+//   });
+// }
+
+// async function message() {
+//   console.log("iam First");
+//   let greet = await greetings();
+//   console.log(greet);
+//   console.log("iam last");
+// }
+
+// message();
+
+// function greetings() {
+//   setTimeout(() => {
+//     console.log("Welcome to Awdiz");
+//   }, 3000);
+// }
+
+// function message() {
+//   console.log("iam First");
+//   let greet = greetings();
+//   console.log(greet); //undefined
+//   console.log("iam last");
+// }
+
+// message();
+
+// function greetings() {
+//   setTimeout(() => {
+//     console.log("Welcome to Awdiz");
+//   }, 3000);
+// }
+// function students() {
+//   setTimeout(() => {
+//     console.log("new to Awdiz");
+//   }, 3500);
+// }
+
+// function message() {
+//   console.log("iam First");
+//   greetings();
+//   students();
+//   console.log("iam last");
+// }
+
+// message();
+
+// function greetings() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("Welcome to Awdiz");
+//     }, 5000);
+//   });
+// }
+// function students() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("students of Awdiz");
+//     }, 2000);
+//   });
+// }
+
+// async function message() {
+//   console.log("iam First");
+//   let std = await students();
+//   console.log(std);
+//   let greet = await greetings();
+//   console.log(greet);
+//   console.log("iam last");
+// }
+
+// message();
